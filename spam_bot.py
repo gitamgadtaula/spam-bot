fee5c663e10f1509075fab9d0a3b2c8fb9297fc1
gitam gadtaula <gitamgadtaula@gmail.com>
2022-09-23 10:50:35 +0545

    loop for every page added

--- spam_bot.py
+++ spam_bot.py
@@ -1,19 +1,19 @@
 import pyautogui
 import time
 from PyPDF2 import PdfReader
-
 reader = PdfReader("script.pdf")
-page = reader.pages[0]
-# print(page.extract_text())
+totalPages = reader.numPages
 
 
 def startSpamming():
-    print('place your cursor active to wherever you want to spam in 5 seconds')
-    time.sleep(5)
-    text = page.extract_text()
-    for each_line in text:
-        pyautogui.typewrite(each_line)
-        pyautogui.press('enter')
+    for i in range(0, totalPages):
+        page = reader.pages[i]
+        print('place your cursor active to wherever you want to spam in 5 seconds')
+        time.sleep(5)
+        text = page.extract_text()
+        for each_line in text:
+            pyautogui.typewrite(each_line)
+            pyautogui.press('enter')
 
 
 startSpamming()
